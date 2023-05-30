fbb5c29c8585a180af537ab85ce8456ebad5914e
MarcoMilos <marcomiloslavich03@gmail.com>
2023-05-29 23:32:47 -0600

    Add board.h

---
+++ board.h
@@ -0,0 +1,13 @@
+// Marco & Alejandro Miloslavich Airola (A01424280, A01424120)
+// 29-05-2023
+// Polimorfismo y Herencia - POO
+
+#include <iostream>
+#include <cstdlib> // sirve para llamar a la funcion rand()
+#pragma once // compila la clase una sola vez
+
+class Board 
+{
+        
+};
+

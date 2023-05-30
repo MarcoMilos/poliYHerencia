dddb520c067fd561bce6090058f5d74eab9c728f
MarcoMilos <marcomiloslavich03@gmail.com>
2023-05-30 12:10:33 -0600

    Add start_constructor

--- match.h
+++ match.h
@@ -4,14 +4,12 @@
 
 #include <cstdlib>
 #include <string>
+#include <iostream>
+#include "dice.h"
 #pragma once
 
 class Match
 {
-    // atributos privados de la clase
-    int currentPlayer = 1; // establece que el primero en jugar es el Player1
-    int turnCount = 1; // establece la cuenta de turnos en 1 al iniciar el juego
-
     // metodos de la clase
     public:
         // constructores
@@ -21,6 +19,40 @@ class Match
         // destructor
         ~Match();
 
-        
+        // starter
+        void start();
 };
 
+void start()
+{
+    int currentPlayer = 1; // establece que el primero en jugar es el Player1
+    int turnCount = 1; // establece la cuenta de turnos en 1 al iniciar el juego
+    int tiles;
+    int snakes;
+    int ladders;
+    int penalty;
+    int reward;
+    int players;
+    int turns;
+    std::string gameType;
+
+    // solicito la cantidad de casillas, serpientes y escaleras desea tener el jugador en su partida
+    std::cout << "Indicate the amount of tiles you wish to have: ";
+    std::cin >> tiles;
+    std::cout << "Indicate the amount of snakes you wish to have: ";
+    std::cin >> snakes;
+    std::cout << "Indicate the amount of ladders you wish to have: ";
+    std::cin >> ladders;
+    std::cout << "Indicate the amount of tiles you will be punished with after falling in a snake tile: ";
+    std::cin >> penalty;
+    std::cout << "Indicate the amount of tiles you will be rewarded with after falling in a ladder tile: ";
+    std::cin >> reward;
+    std::cout << "Indicate the amount of players in the match: ";
+    std::cin >> players;
+    std::cout << "Indicate the maximum amount of turns for the game: ";
+    std::cin >> turns;
+    std::cout << "Indicate the game type A <automatic>, M <manual>: ";
+    std::cin >> gameType;
+
+    
+}

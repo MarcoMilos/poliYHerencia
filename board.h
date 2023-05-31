7ddecbabbb84b1b85372a2bee2c378a7768be2bc
alejandroMilos <alejandromilospersonal@gmail.com>
2023-05-30 22:35:07 -0600

    Modif classBoard

--- board.h
+++ board.h
@@ -4,11 +4,130 @@
 
 #include <iostream>
 #include <cstdlib> // sirve para llamar a la funcion rand()
-#include "match.h"
+#include <vector>
+#include "tile.h"
+#include "snake.h"
+#include "ladder.h"
 #pragma once // compila la clase una sola vez
 
 class Board 
 {
-        
+    // atributos de clase
+    int tiles = 0; // cantidad de casillas por omision
+    int snakes = 0; // cantidad de serpientes por omision
+    int ladders = 0; // cantidad de escaleras por omision
+    std::vector<Tile *> boardTiles;
+    
+    public:
+        // constructores
+        Board() = default; // constructor por omision
+        Board(int tiles, int snakes, int ladders); // constructor por definicion
+
+        // destructor
+        ~Board();
+
+        // setters
+        void setTiles(int tiles);
+        void setSnakes(int snakes);
+        void setLadders(int ladders);
+
+        // getters
+        const int getTiles();
+        const int getSnakes();
+        const int getLadders();
+
+        // metodo para crear el tablero
+        void createBoard();
+
+        // metodo para imprimir el tablero
+        virtual void print();
 };
 
+Board::Board(int tiles, int snakes, int ladders) : tiles(tiles), snakes(snakes), ladders(ladders)
+{
+    std::cout << "Indicate the amount of tiles you wish to have: ";
+    std::cin >> tiles;
+    std::cout << "Indicate the amount of snakes you wish to have: ";
+    std::cin >> snakes;
+    std::cout << "Indicate the amount of ladders you wish to have: ";
+    std::cin >> ladders;
+
+    createBoard();
+}
+
+Board::~Board()
+{ 
+    for (Tile* tile : boardTiles)
+    {
+        delete tile;
+    }
+}
+
+void Board::setTiles(int tiles)
+{
+    this->tiles = tiles;
+}
+
+void Board::setSnakes(int snakes)
+{
+    this->snakes = snakes;
+}
+
+void Board::setLadders(int ladders)
+{
+    this->ladders = ladders;
+}
+
+const int Board::getTiles()
+{
+    return tiles;
+}
+
+const int Board::getSnakes()
+{
+    return snakes;
+}
+
+const int Board::getLadders()
+{
+    return ladders;
+}
+
+void Board::createBoard()
+{
+    // eliminar la configuracion previa del tablero
+    boardTiles.clear();
+
+    // creamos las casillas normales
+    for (int i = 0; i < tiles; i++)
+    {
+        Tile* tile = new Tile(i, 'N');
+        boardTiles.push_back(tile);
+    }
+
+    // creamos las casillas de tipo serpiente
+    for (int i = 0; i < snakes; i++)
+    {
+        int randomTile = rand() % tiles;
+        Tile* snake = new Snake(randomTile, 'S');
+        boardTiles[randomTile] = snake;
+    }
+
+    // creamos las casillas de tipo escalera
+    for (int i = 0; i < ladders; i++)
+    {
+        int randomTile = rand() % tiles;
+        Tile* ladder = new Ladder(randomTile, 'L');
+        boardTiles[randomTile] = ladder;
+    }
+}
+
+void Board::print()
+{
+    for (Tile* tile : boardTiles)
+    {
+        tile->print();
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+}

e7c228f6d31ba4e79a9e530a2f136cf88209ae56
MarcoMilos <marcomiloslavich03@gmail.com>
2023-05-30 18:22:41 -0600

    Modif classLadder

--- ladder.h
+++ ladder.h
@@ -9,5 +9,22 @@
 
 class Ladder : public Tile
 {
+    public:
+        // constructores
+        Ladder() = default; // constructor por omision
+        // constructor de formato inline para inicializar los dos atributos de la clase padre Tile
+        Ladder(const int &tileValue, const char &tileType) : Tile(tileValue, tileType) {}
 
+        // destructor
+        ~Ladder();
+
+        // metodo de impresion
+        void print();
 };
+
+Ladder::~Ladder(){}
+
+void Ladder::print()
+{
+    std::cout << 'L' << getType();
+}
